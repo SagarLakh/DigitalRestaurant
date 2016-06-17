7ec958860f581d600b72c47fbdf58ac446d59730
Xavi Sanchez Mir <xavi.sanchez.mir@gmail.com>
2016-06-17 19:48:33 +0200

    Adding REST API

---
+++ server/REST.js
@@ -0,0 +1,50 @@
+var mysql = require("mysql");
+function REST_ROUTER(router,connection,md5) {
+    var self = this;
+    self.handleRoutes(router,connection,md5);
+}
+
+REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
+    router.get("/",function(req,res){
+        res.json({"Message" : "Hello World !"});
+    })
+    router.post("/users",function(req,res){
+        var query = "INSERT INTO ??(??,??,??,??,??,??) VALUES (?,?,?,?,?,?)";
+        var table = ["User","name","surname","email","username","password","sex",req.body.name,req.body.surname,req.body.email,req.body.username,md5(req.body.password),req.body.sex];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "User Added !"});
+            }
+        });
+    });
+    router.get("/users",function(req,res){
+        var query = "SELECT * FROM ??";
+        var table = ["User"];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+            }
+        });
+    });
+
+    router.get("/users/:id_user",function(req,res){
+        var query = "SELECT * FROM ?? WHERE ??=?";
+        var table = ["User","id_user",req.params.id_user];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+            }
+        });
+    });
+}
+
+module.exports = REST_ROUTER;
--- server/app.js
+++ server/app.js
@@ -1,10 +1,54 @@
-var express = require('express');
-var app = express();
+var express = require("express");
+var mysql   = require("mysql");
+var bodyParser  = require("body-parser");
+var md5 = require('MD5');
+var rest = require("./REST.js");
+var app  = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+function REST(){
+    var self = this;
+    self.connectMysql();
+};
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+REST.prototype.connectMysql = function() {
+    var self = this;
+    var pool      =    mysql.createPool({
+        connectionLimit : 100,
+        host     : 'localhost',
+        port     : '3306',
+        user     : 'root',
+        password : 'root',
+        database : 'EssenEasy',
+        debug    :  false
+    });
+
+    pool.getConnection(function(err,connection){
+        if(err) {
+          self.stop(err);
+        } else {
+          self.configureExpress(connection);
+        }
+    });
+}
+REST.prototype.configureExpress = function(connection) {
+      var self = this;
+      app.use(bodyParser.urlencoded({ extended: true }));
+      app.use(bodyParser.json());
+      var router = express.Router();
+      app.use('/api', router);
+      var rest_router = new rest(router,connection,md5);
+      self.startServer();
+}
+
+REST.prototype.startServer = function() {
+      app.listen(3000,function(){
+          console.log("All right ! I am alive at Port 3000.");
+      });
+}
+
+REST.prototype.stop = function(err) {
+    console.log("ISSUE WITH MYSQL " + err);
+    process.exit(1);
+}
+
+new REST();
